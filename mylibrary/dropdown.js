c83183eba2fb15b1f2fbdc1a3c33f6e0f5c82d19
Randold8 <84187343+Randold8@users.noreply.github.com>
2021-07-22 17:40:42 +0300

    asdsada

---
+++ mylibrary/dropdown.js
@@ -0,0 +1,25 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+let imgae = document.querySelector('.dropimg');
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+let arrow = document.querySelector('.arrow');
+window.onscroll = function () {
+  if (window.pageYOffset > 300) {
+    arrow.style.display = "none";
+  } else {
+    arrow.style.display = "block"
+  };
+}
---
+++ mylibrary/filter.js
@@ -0,0 +1,16 @@
+let sels = document.querySelectorAll('.sel');
+let mainsel = document.querySelector('.dropbtn');
+let books = document.querySelectorAll('.book');
+for (let sel of sels) {
+    sel.onclick = function () {
+        mainsel.textContent = sel.textContent
+        for (let book of books) {
+            if (sel.textContent !== book.dataset.topic && sel.textContent !== "All books") {
+                book.style.display = "none";
+            } else {
+                book.style.display = "block";
+            }
+        }
+
+    }
+}
