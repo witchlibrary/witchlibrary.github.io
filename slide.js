fccbcd0036f3dedff83931a9f46d5cb6dade8594
Randold8 <84187343+Randold8@users.noreply.github.com>
2021-07-26 23:27:33 +0300

    asddddddddddddd

---
+++ slide.js
@@ -0,0 +1,30 @@
+let sliders = document.querySelectorAll( '.bookinfo' );
+let covers = document.querySelectorAll( '.bookcover' );
+var heights = document.querySelectorAll('.bookinfo');
+var heightlist = [];
+for (let newheight of heights) {
+newheight = newheight.offsetHeight;
+heightlist.push(newheight);
+}
+for (let slider of sliders) {
+    slider.style.height = "0px";
+}
+
+    covers.forEach (function(cover, i) {
+        cover.addEventListener( 'click', function() {
+         
+            if (sliders[i].style.height == "0px") {
+                sliders.forEach(function(slider){
+                    slider.style.height = "0px";
+                })
+                sliders[i].style.height = heightlist[i] + "px";
+
+            } else {
+                sliders[i].style.height = "0px";
+
+            }
+            
+            
+        }, false );
+        });
+        
